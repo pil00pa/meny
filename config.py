bdb6516ba43937d18834190f1386e9506563aa25
pil00pa <valeriy.mikhaylov2003@gmail.com>
2023-01-04 17:56:59 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+# TOKEN = str(os.getenv('TOKEN'))
+
+TOKEN = '1855055611:AAGbmzKE3BJcRFL85b4h9USXXaLWSaHwoa0'
+STARTER_TABLE = 'table_for_sudoku.jpg'
+TABLE = 'sudoku.jpg'
+FONT = 'CeraPro-Light.ttf'
